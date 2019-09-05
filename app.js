1d6a721432ff2b45dce541a3b08cd98f8c2777c0
amitsinha1979@gmail.com <amit.d.sinha@accenture.com>
2019-09-06 03:11:23 +0530

    Added Static file

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 var express = require("express");
 var app = new express();
 var port = 3000;
+
 //Following function is starting socket and start listen from particular port.
 app.listen(port, function(err){
   if(typeof(err) == "undefined"){
     console.log("Your application is running on:" + port + "port");
   }
 });
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/', function(req, res){
   res.send('<h1>Hello C# Corner.</h1>');
